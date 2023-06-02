c74732ade3930c24f66ebd09323de33bbdd6a5ef
Dan Holgate <Dan.Holgate@agileautomations.co.uk>
2023-06-02 17:04:21 +0100

    .

--- build/manager.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class MessagesManager {
-    static SetupMsgData(msgData: any, messagesFound: any): {
-        MessageData: any;
-        ConversationsNeeded: any;
-    };
-    static SetupConvData(msgData: any, messagesFound: any, messages: any): Promise<any>;
-}

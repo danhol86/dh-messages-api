45763982b1a02f5987d909988aa15eab582795a9
Dan Holgate <Dan.Holgate@agileautomations.co.uk>
2023-06-07 10:53:31 +0100

    .

--- build/json.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class JsonFunctions {
-    static dateObj: (number | null)[];
-    static getRecMessagesStringJSON(reqid: any, b64: any, subnum: any): any[][];
-    static getAckMessagesStringJSON(newguid: any, guids: any, n64: any, subnum: any): any[];
-    static getSendMessagesStringJSON(bugle: any, respguid: any, respauth: any, sendproto64: any): any[];
-    static GetRefreshTokenJSON(guid: any, oldtachyon: any, bugle: any, unixtimestamp: any, signedcode: any): any[];
-}
